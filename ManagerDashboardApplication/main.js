fb89e58adb40acefd7ad23e6fd6ee5d34ff87512
Trinh Quy Thien <trinhquythien.dev@gmail.com>
2024-03-12 12:04:15 +0700

    update_2024_03_12_1

--- ManagerDashboardApplication/main.js
+++ ManagerDashboardApplication/main.js
@@ -18,8 +18,20 @@ function changeColor() {
     if (cB >= 255.0 || cB <= 0.0) {
         iB = -iB
     }
-    console.log(rgb(cR, cG, cB))
+    //console.log(rgb(cR, cG, cB))
     document.getElementById("button_troll").style.color = rgb(cR, cG, cB)
 }
 
+var names = ['Trịnh Quý Thiện', 'Trần Viết Thịnh', 'Đặng Văn Nhớ', 'Nguyễn Thành Nhân', 'Lưu Văn Trường']
+var currentName = 0;
+
+function changeName() {
+    currentName += 1
+    if (currentName == 5)
+        currentName = 0
+    //console.log(currentName)
+    document.getElementById("name").textContent = names[currentName]
+}
+
+setInterval(changeName, 100)
 setInterval(changeColor, 10)
