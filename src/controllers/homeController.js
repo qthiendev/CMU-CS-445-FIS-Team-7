cd0ff4c1ca21c9ebf57b0c331dbd3cfe0763de5e
Trinh Quy Thien <trinhquythien.dev@gmail.com>
2024-04-17 13:47:46 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,6 +6,24 @@ const getHomepage = async (req, res) => {
     return res.render('homePage.ejs',{getHR})
 }
 
+const recordsetFilter = async (req, res) => {
+
+    try {
+        let shareholder = req.body.shareholder;
+        let gender = req.body.gender;
+        let user = req.body.user;
+        let department = req.body.department;
+        let workType = req.body.workType;
+
+        console.log("filter shareholder: ", shareholder)
+    }
+    catch (error){
+        console.error(error);
+    }
+}
+
+recordsetFilter();
+
 module.exports={
-    getHomepage, 
+    getHomepage, recordsetFilter
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routers/web.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());       
 app.use(express.urlencoded({extended: true})); 
 configViewEngine(app);
