8b30e162fa38e3f77e500ed6eb0966ed8ae5113e
Trinh Quy Thien <trinhquythien.dev@gmail.com>
2024-04-18 22:59:24 +0700

    update_3

--- src/controllers/rVacationDayController.js
+++ src/controllers/rVacationDayController.js
@@ -2,11 +2,11 @@ const { querryHRDB } = require('../service/querryHRDB');
 
 const getReportVD = async (req, res) => {
     try {
-        let shareholder = req.body.shareholder;
+        let shareholder = req.query.shareholder;
         let gender = req.query.gender;
-        let user = req.body.user;
-        let department = req.body.department;
-        let workType = req.body.workType;
+        let user = req.query.user;
+        let department = req.query.department;
+        let workType = req.query.workType;
 
         var data = await querryHRDB();
         // let filteredData = data.filter(record => 
@@ -16,11 +16,23 @@ const getReportVD = async (req, res) => {
         //     (department == null || record.department === department) &&
         //     (workType == null || record.workType === workType)
         //   );
-      
-        console.log('data:', data);
+        console.log(shareholder);
+        if (shareholder !== '' && shareholder !== undefined){
+            data = data.filter(record => record.SHAREHOLDER_STATUS == shareholder)
+        }
+        if (gender !== '' && gender !== undefined){
+            data = data.filter(record => record.SHAREHOLDER_STATUS == shareholder)
+        }
+        if (user !== '' && user !== undefined){
+            data = data.filter(record => record.SHAREHOLDER_STATUS == shareholder)
+        }
+        if (shareholder !== '' && shareholder !== undefined){
+            data = data.filter(record => record.SHAREHOLDER_STATUS == shareholder)
+        }
+        if (shareholder !== '' && shareholder !== undefined){
+            data = data.filter(record => record.SHAREHOLDER_STATUS == shareholder)
+        }
         res.render("rVacationDayPage.ejs", { data } );
-        console.log('Gender>>>>>>>>>:', gender);
-        console.log("Req.BODY>>>>",req.query);
     }
     catch (err) {
         console.log(err);
