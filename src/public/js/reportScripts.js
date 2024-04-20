3e153e9f46b19bd5c1fc262ed265e6f4a279d80b
dangvannho <dangvannho7@gmail.com>
2024-04-20 22:23:17 +0700

    update 20/4/2022

--- src/public/js/reportScripts.js
+++ src/public/js/reportScripts.js
@@ -1,36 +1,46 @@
 const btnList = document.querySelector(".btn.btn-list");
-const subnav = document.querySelector(".sub-nav");
-const arrow = document.querySelector(".bx-chevron-down");
+const btnList_2 = document.querySelector(".btn-list-2");
+const subnav = document.querySelector(".sub-nav-1");
+console.log(subnav);
+const subnav_2 = document.querySelector(".sub-nav-2");
+const arrow = document.querySelector(".bx-chevron-down-1");
+const arrow_2 = document.querySelector(".bx-chevron-down-2");
 
 btnList.addEventListener("click", () => {
-  arrow.classList.toggle("rotate");
   subnav.classList.toggle("active");
+  arrow.classList.toggle("rotate");
 });
+
+btnList_2.addEventListener("click", () => {
+  subnav_2.classList.toggle("active");
+  arrow_2.classList.toggle("rotate");
+});
+
 function createClock() {
-  const clockElement = document.createElement('div');
-  clockElement.id = 'clock';
+  const clockElement = document.createElement("div");
+  clockElement.id = "clock";
   document.body.appendChild(clockElement);
 }
 
 // Function to update clock time
 function updateClock() {
   const now = new Date();
-  document.getElementById('clock').textContent = formatTime(now);
+  document.getElementById("clock").textContent = formatTime(now);
 }
 
 // Function to format time
 function formatTime(date) {
   const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
     hour12: false,
-    timeZone: 'Asia/Ho_Chi_Minh' // Set time zone to Vietnam
+    timeZone: "Asia/Ho_Chi_Minh", // Set time zone to Vietnam
   };
-  return new Intl.DateTimeFormat('en-GB', options).format(date);
+  return new Intl.DateTimeFormat("en-GB", options).format(date);
 }
 
 // Create and update clock
