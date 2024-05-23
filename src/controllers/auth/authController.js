dfcb9b10ee7b088497e6fa61d8ab5b20984e364a
dangvannho <dangvannho7@gmail.com>
2024-05-23 08:03:08 +0700

    update ma hoa password

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -1,44 +1,41 @@
-const Accounts=require("../../models/accounts");
-
+const Accounts = require("../../models/accounts");
+const md5 = require("md5");
 class index {
-    //[GET]/auth/login
-    async login(req,res){
-        res.render("loginPage.ejs")
-           
+  //[GET]/auth/login
+  async login(req, res) {
+    res.render("loginPage.ejs");
+  }
+
+  async homePage(req, res) {
+    res.render("homePage.ejs");
+  }
+
+  async loginPost(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await Accounts.findOne({
+      email: email,
+      deleted: false,
+    });
+
+    if (!user) {
+      res.redirect("back");
+      return;
     }
 
-    async homePage(req,res){
-        res.render("homePage.ejs")
- 
+    if (md5(password) != user.password) {
+      res.redirect("back");
+      return;
     }
+    res.cookie("token", user.token);
+    res.redirect("homePage");
+  }
 
-    async loginPost(req,res){
-        const email=req.body.email
-        const password=req.body.password
-
-
-        const user = await Accounts.findOne({
-            email : email,
-            deleted : false
-        })
-
-        if(!user){
-            res.redirect("back")
-            return
-        }   
-
-        if(password != user.password){
-            res.redirect("back")
-            return
-        }
-        res.cookie("token", user.token)
-        res.redirect("homePage")
-    }
-
-    logout(req,res){
-        res.clearCookie("token")
-        res.redirect(`login`)
-    }
+  logout(req, res) {
+    res.clearCookie("token");
+    res.redirect(`login`);
+  }
 }
 
-module.exports = new index
+module.exports = new index();
