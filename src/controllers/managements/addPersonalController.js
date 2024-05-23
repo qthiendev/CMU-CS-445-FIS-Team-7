0dac95ee69c8b4dcc7cdd9016db80be8f961709d
Trinh Quy Thien <trinhquythien.dev@gmail.com>
2024-05-23 12:18:27 +0700

    update

--- src/controllers/managements/addPersonalController.js
+++ src/controllers/managements/addPersonalController.js
@@ -26,42 +26,46 @@ const addNewPersonalInformation = async (req, res) => {
             BENEFIT_PLANS_ID
         } = req.query;
 
-        if (CURRENT_FIRST_NAME 
+        if (CURRENT_FIRST_NAME
             && CURRENT_LAST_NAME
             && SOCIAL_SECURITY_NUMBER) {
-                await addPersonalInformation(PERSONAL_ID,
-                    CURRENT_FIRST_NAME,
-                    CURRENT_LAST_NAME,
-                    CURRENT_MIDDLE_NAME,
-                    BIRTH_DATE,
-                    SOCIAL_SECURITY_NUMBER,
-                    DRIVERS_LICENSE,
-                    CURRENT_ADDRESS_1,
-                    CURRENT_ADDRESS_2,
-                    CURRENT_CITY,
-                    CURRENT_COUNTRY,
-                    CURRENT_ZIP,
-                    CURRENT_GENDER,
-                    CURRENT_PHONE_NUMBER,
-                    CURRENT_PERSONAL_EMAIL,
-                    CURRENT_MARITAL_STATUS,
-                    ETHNICITY,
-                    SHAREHOLDER_STATUS,
-                    BENEFIT_PLANS_ID);
-        
-                console.log('[System] employeesManagementController.js | Added new PERSONAL:'   );
-                let data = await getInformationByPersonalID(PERSONAL_ID);
-                
-                res.render(
-                    "manageInformationSpecificPage.ejs",
-                    {
-                        data,
-                        id: PERSONAL_ID
-                    }
-                );
+            await addPersonalInformation(PERSONAL_ID,
+                CURRENT_FIRST_NAME,
+                CURRENT_LAST_NAME,
+                CURRENT_MIDDLE_NAME,
+                BIRTH_DATE,
+                SOCIAL_SECURITY_NUMBER,
+                DRIVERS_LICENSE,
+                CURRENT_ADDRESS_1,
+                CURRENT_ADDRESS_2,
+                CURRENT_CITY,
+                CURRENT_COUNTRY,
+                CURRENT_ZIP,
+                CURRENT_GENDER,
+                CURRENT_PHONE_NUMBER,
+                CURRENT_PERSONAL_EMAIL,
+                CURRENT_MARITAL_STATUS,
+                ETHNICITY,
+                SHAREHOLDER_STATUS,
+                BENEFIT_PLANS_ID);
+
+            console.log('[System] employeesManagementController.js | Added new PERSONAL:');
+            let data = await getInformationByPersonalID(PERSONAL_ID);
+
+            res.render(
+                "manageInformationSpecificPage.ejs",
+                {
+                    data,
+                    id: PERSONAL_ID
+                }
+            );
         } else {
-            console.log('[System] employeesManagementController.js | Cannot Added new PERSONAL');
-            res.redirect('/Information');
+            console.log('[System] employeesManagementController.js | Cannot Add new PERSONAL');
+            // Redirect to add page with existest feild
+            const queryString = Object.keys(req.query)
+                .map(key => `${key}=${encodeURIComponent(req.query[key])}`)
+                .join('&');
+            res.redirect(`/Information/Add?${queryString}&message=Must fill * fields`);
         }
     } catch (err) {
         console.log('[System] employeesManagementController.js | Cannot Added new PERSONAL:', err);
